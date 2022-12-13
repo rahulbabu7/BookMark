aebbe0f84b89804772a8bba2ddfa9ea18a5aa862
rahulbabu7 <rahulbabu436@gmail.com>
2022-12-13 18:42:53 +0530

    new

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],//telling the html folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
